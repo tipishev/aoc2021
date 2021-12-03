afea49da3b319a86623a7e7d6644e9b798c46da9
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-03 21:50:14 +0100

    add a shell-friendly solve function

--- src/aoc2021.erl
+++ src/aoc2021.erl
@@ -1,24 +1,31 @@
 -module(aoc2021).
 
 %% API exports
--export([main/1]).
+-export([main/1, solve/3]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+%% @doc a wrapper around solve/3, can be run as escript
 main([DayStr, PartStr]) ->
     main([DayStr, PartStr, "real"]);
-main([DayStr, PartStr, InputType]) ->
-    io:format("Solving ~s ~s with ~s input.~n", [DayStr, PartStr, InputType]),
+main([DayStr, PartStr, InputTypeStr]) ->
+    io:format("Solving ~s ~s with ~s input.~n", [DayStr, PartStr, InputTypeStr]),
     Day = list_to_atom(DayStr),
     Part = list_to_atom(PartStr),
-    InputFilename =
-        case InputType of
-            "example" -> DayStr ++ "_example.txt";
-            "real" -> DayStr ++ ".txt"
-        end,
-    InputFilePath = filename:join("inputs", InputFilename),
-    Result = Day:Part([InputFilePath]),
+    InputType = list_to_atom(InputTypeStr),
+    Result = solve(Day, Part, InputType),
     io:format("Result: ~p~n", [Result]),
     erlang:halt(0).
+
+%% @doc an interactive shell function
+solve(Day, Part, InputType) ->
+    DayStr = atom_to_list(Day),
+    InputFilename =
+        case InputType of
+            example -> DayStr ++ "_example.txt";
+            real -> DayStr ++ ".txt"
+        end,
+    InputFilePath = filename:join("inputs", InputFilename),
+    Day:Part([InputFilePath]).
