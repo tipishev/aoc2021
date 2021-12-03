4329c3ffdbc916b4e083f51bb90863b80f60851d
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-04 00:23:31 +0100

    refactor part1

--- src/day3.erl
+++ src/day3.erl
@@ -11,17 +11,15 @@ part1(File) ->
     GammaDec * EpsilonDec.
 
 gamma_epsilon(Binaries) ->
-    [CountsAcc | CountsTail] = [count(Binary) || Binary <- Binaries],
-    ReducedCounts = lists:foldl(fun sum/2, CountsAcc, CountsTail),
-    Gamma = [most_common(Count) || Count <- ReducedCounts],
+    Columns = transpose(Binaries),
+    Gamma = [most_common_bit(Column) || Column <- Columns],
     Epsilon = inverse(Gamma),
     {Gamma, Epsilon}.
 
-count(List) ->
-    [increment(Element, {0, 0}) || Element <- List].
-
-increment(0, {CountZero, CountOne}) -> {CountZero + 1, CountOne};
-increment(1, {CountZero, CountOne}) -> {CountZero, CountOne + 1}.
+%% stolen from https://stackoverflow.com/a/7855826
+transpose([[]|_]) -> [];
+transpose(M) ->
+    [lists:map(fun hd/1, M) | transpose(lists:map(fun tl/1, M))].
 
 bin_to_dec(Bin) ->
     Indices = lists:seq(0, length(Bin) - 1),
@@ -32,15 +30,6 @@ inverse(Bin) when is_list(Bin) -> [inverse(Element) || Element <- Bin];
 inverse(0) -> 1;
 inverse(1) -> 0.
 
-sum(CountsA, CountsB) when is_list(CountsA) andalso is_list(CountsB) ->
-    [sum(AtPosA, AtPosB) || {AtPosA, AtPosB} <- lists:zip(CountsA, CountsB)];
-sum({CountZeroA, CountOneA}, {CountZeroB, CountOneB}) ->
-    {CountZeroA + CountZeroB, CountOneA + CountOneB}.
-
-most_common({CountZero, CountOne}) when CountZero > CountOne -> 0;
-most_common({CountZero, CountOne}) when CountZero < CountOne -> 1;
-most_common({Same, Same}) -> 1.
-
 part2(File) ->
     Binaries = read_binaries(File),
     Oxy = oxy(Binaries),
@@ -50,8 +39,8 @@ part2(File) ->
 oxy(Binaries) -> filter(oxy, Binaries).
 scrub(Binaries) -> filter(scrub, Binaries).
 
-filter(oxy, Binaries) -> filter(Binaries, fun mode/1);
-filter(scrub, Binaries) -> filter(Binaries, fun inverse_mode/1);
+filter(oxy, Binaries) -> filter(Binaries, fun most_common_bit/1);
+filter(scrub, Binaries) -> filter(Binaries, fun least_common_bit/1);
 filter(Binaries, ModeFun) -> filter(1, Binaries, ModeFun).
 
 filter(_Pos, [TheOne], _ModeFun) -> bin_to_dec(TheOne);
@@ -61,20 +50,17 @@ filter(Pos, Binaries, ModeFun) ->
     Filtered = [Binary || Binary <- Binaries, lists:nth(Pos, Binary) =:= Mode],
     filter(Pos + 1, Filtered, ModeFun).
 
-%% @doc finds the most common bit in a binary
-mode(Binary) ->
+most_common_bit(Binary) ->
     Sum = lists:sum(Binary),
     Length = length(Binary),
-    mode(Sum, Length).
-mode(Sum, Length) when Sum >= Length/2 -> 1;
-mode(_, _) -> 0.
+    most_common_bit(Sum, Length).
+most_common_bit(Sum, Length) when Sum >= Length/2 -> 1;
+most_common_bit(_, _) -> 0.
 
-inverse_mode(Binary) -> inverse(mode(Binary)).
-
-
+least_common_bit(Binary) -> inverse(most_common_bit(Binary)).
 
 %%====================================================================
-%% Santa's little helpers
+%% Santa's little parsers
 %%====================================================================
 
 read_binaries(Filename) ->
