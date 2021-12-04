28886ede9fa0ad14e108f9b49377764f19c678a5
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-04 09:03:02 +0100

    add day4 parser

--- src/day4.erl
+++ src/day4.erl
@@ -11,5 +11,28 @@ part2(File) ->
 
 parse(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
-    Lines = string:lexemes(FileContent, "\n"),
-    Lines.
+    [HeaderStr, BoardsStr] = string:split(FileContent, "\n\n"),
+    DrawnNumbers = parse_header(HeaderStr),
+    Boards = parse_boards(BoardsStr),
+    {DrawnNumbers, Boards}.
+
+parse_boards(BoardsStr) -> parse_boards(BoardsStr, _Boards = []).
+parse_boards(BoardsStr, Boards) ->
+    case string:find(BoardsStr, "\n\n") of
+        nomatch ->
+            TheLastBoard = parse_board(BoardsStr),
+            lists:reverse([TheLastBoard | Boards]);
+        _MoreThanOneBoard ->
+            [BoardStr, RemainingBoardStr] = string:split(BoardsStr, "\n\n"),
+            Board = parse_board(BoardStr),
+            parse_boards(RemainingBoardStr, [Board | Boards])
+    end.
+
+parse_board(BoardStr) ->
+    Rows = string:lexemes(BoardStr, "\n"),
+    [[binary_to_integer(Value)
+      || Value <- string:lexemes(Row, " ")] || Row <- Rows].
+
+parse_header(HeaderStr) ->
+    DrawnNumbers = string:lexemes(HeaderStr, ","),
+    [binary_to_integer(DrawnNumber) || DrawnNumber <- DrawnNumbers].
