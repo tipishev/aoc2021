f1b0a4269afdc04a25ade8e0a20ad14fbf0a29bf
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-04 21:36:47 +0100

    add day5 stubs

---
+++ src/day5.erl
@@ -0,0 +1,15 @@
+-module(day5).
+
+%% API exports
+-export([part1/1, part2/1]).
+
+part1(File) ->
+    parse(File).
+
+part2(File) ->
+    parse(File).
+
+parse(Filename) ->
+    {ok, FileContent} = file:read_file(Filename),
+    Lines = string:lexemes(FileContent, "\n"),
+    Lines.
