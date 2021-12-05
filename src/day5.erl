1e51c54e0337725ed79832daa998d13c41cef5fe
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-05 13:48:12 +0100

    add day5 parser

--- src/day5.erl
+++ src/day5.erl
@@ -4,12 +4,29 @@
 -export([part1/1, part2/1]).
 
 part1(File) ->
-    parse(File).
+    Vents = parse_vectors(File),
+    Vents.
 
 part2(File) ->
-    parse(File).
+    parse_vectors(File).
 
-parse(Filename) ->
+
+%%% Parser
+
+parse_vectors(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
-    Lines = string:lexemes(FileContent, "\n"),
-    Lines.
+    StrVectors = string:lexemes(FileContent, "\n"),
+    [parse_vector(StrVector) || StrVector <- StrVectors].
+
+parse_vector(StrVector) ->
+    [StrPointA, <<"->">>, StrPointB] = string:lexemes(StrVector, " "),
+    PointA = parse_point(StrPointA),
+    PointB = parse_point(StrPointB),
+    {PointA, PointB}.
+
+parse_point(StrPoint) ->
+    [StrX, StrY] = string:lexemes(StrPoint, ","),
+    X = binary_to_integer(StrX),
+    Y = binary_to_integer(StrY),
+    {X, Y}.
+
