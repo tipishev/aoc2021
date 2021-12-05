f86e02eea9409337e95b9b18d2b4f8a8af1632f1
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-05 15:49:39 +0100

    solve day 5 part 1

--- src/day5.erl
+++ src/day5.erl
@@ -4,12 +4,49 @@
 -export([part1/1, part2/1]).
 
 part1(File) ->
+    Board = board_new(),
     Vents = parse_vectors(File),
-    Vents.
+    NonDiagonalVents = lists:filter(fun non_diagonal/1, Vents),
+    CoveredSquares = covered_squares(NonDiagonalVents),
+    CoveredBoard = board_increment(Board, CoveredSquares),
+    board_count_overlaps(CoveredBoard).
 
 part2(File) ->
     parse_vectors(File).
 
+%%% Board helpers
+
+board_new() -> #{}.
+board_increment(Board, Squares) when is_list(Squares)->
+    lists:foldl(fun(Square, BoardAcc) -> board_increment(BoardAcc, Square) end, Board, Squares);
+board_increment(Board, Square) when is_tuple(Square)->
+    Increment = fun(V) -> V + 1 end,
+    maps:update_with(Square, Increment, _Default=1, Board).
+
+board_count_overlaps(Board) ->
+    Counts = maps:values(Board),
+    length([Count || Count <- Counts, Count > 1]).
+
+%%% Vector helpers
+
+non_diagonal(Vector) ->
+    is_horizontal(Vector) orelse is_vertical(Vector).
+
+is_horizontal({{X, _Y1}, {X, _Y2}}) -> true;
+is_horizontal({{_X1, _Y1}, {_X2, _Y2}}) -> false.
+
+is_vertical({{_X1, Y}, {_X2, Y}}) -> true;
+is_vertical({{_X1, _Y1}, {_X2, _Y2}}) -> false.
+
+covered_squares(Vectors) when is_list(Vectors) ->
+    lists:flatten([covered_squares(Vector) || Vector <- Vectors]);
+covered_squares(Vector) when is_tuple(Vector) ->
+    {{X1, Y1}, {X2, Y2}} = Vector,
+    [SmallerX, BiggerX] = lists:sort([X1, X2]),
+    [SmallerY, BiggerY] = lists:sort([Y1, Y2]),
+    Xs = lists:seq(SmallerX, BiggerX),
+    Ys = lists:seq(SmallerY, BiggerY),
+    [{X, Y} || X <- Xs, Y <- Ys].
 
 %%% Parser
 
