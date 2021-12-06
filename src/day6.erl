3ebdd5e4fe193a2d594e6730ca00791db40c62e0
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-06 09:36:22 +0100

    solve day6 part1

--- src/day6.erl
+++ src/day6.erl
@@ -4,7 +4,21 @@
 -export([part1/1, part2/1]).
 
 part1(File) ->
-    parse_int_array(File).
+    Timers = parse_int_array(File),
+    length(advance(Timers, 80)).
+
+advance(Timers, 0) ->
+    Timers;
+advance(Timers, Days) ->
+    advance(advance(Timers), Days - 1).
+
+advance(Timers) when is_list(Timers) ->
+   lists:flatten([advance(Timer) || Timer <- Timers]);
+advance(Timer) when Timer > 0 ->
+    Timer - 1;
+advance(Timer) when Timer =:= 0 ->
+    [6, 8].
+
 
 part2(File) ->
     parse_int_array(File).
