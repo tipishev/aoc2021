7e04402891520e6dcdc208529565303fba13cede
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-07 07:08:01 +0100

    solve day7 part2

--- src/day7.erl
+++ src/day7.erl
@@ -5,23 +5,33 @@
 
 part1(File) ->
     Positions = parse_int_array(File),
-    brute_force_target(Positions).
+    brute_force_target(Positions, fun linear_fuel_cost/2).
 
-brute_force_target(Positions) ->
-    Min = lists:min(Positions),
-    Max = lists:max(Positions),
-    Targets = lists:seq(Min, Max),
-    FuelCosts = [fuel_cost(Positions, Target) || Target <- Targets],
-    lists:min(FuelCosts).
-
-fuel_cost(Positions, Target) when is_list(Positions) ->
-    FuelCosts = [fuel_cost(Position, Target) || Position <- Positions],
+linear_fuel_cost(Positions, Target) when is_list(Positions) ->
+    FuelCosts = [linear_fuel_cost(Position, Target) || Position <- Positions],
     lists:sum(FuelCosts);
-fuel_cost(Position, Target) when is_integer(Position) ->
+linear_fuel_cost(Position, Target) when is_integer(Position) ->
     abs(Position - Target).
 
 part2(File) ->
-    parse_int_array(File).
+    Positions = parse_int_array(File),
+    brute_force_target(Positions, fun progressive_fuel_cost/2).
+
+progressive_fuel_cost(Positions, Target) when is_list(Positions) ->
+    FuelCosts = [progressive_fuel_cost(Position, Target) || Position <- Positions],
+    lists:sum(FuelCosts);
+progressive_fuel_cost(Position, Target) when is_integer(Position) ->
+    Distance = abs(Position - Target),
+    (Distance/2) * ((2 * 1) + (Distance - 1) * 1).
+
+brute_force_target(Positions, FuelCostFun) ->
+    Min = lists:min(Positions),
+    Max = lists:max(Positions),
+    Targets = lists:seq(Min, Max),
+    FuelCosts = [FuelCostFun(Positions, Target) || Target <- Targets],
+    lists:min(FuelCosts).
+
+% Parser
 
 parse_int_array(Filename) ->
     {ok, CsvStrIntegers} = file:read_file(Filename),
