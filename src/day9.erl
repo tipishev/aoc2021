b7dad0d1032b9df97d288ba7887eb21ea7c19ea2
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-09 08:54:42 +0100

    day9 add parser and indexer

--- src/day9.erl
+++ src/day9.erl
@@ -4,12 +4,32 @@
 -export([part1/1, part2/1]).
 
 part1(File) ->
-    parse(File).
+    Rows = read_integer_lists(File),
+    Index = index(Rows),
+    Index.
 
 part2(File) ->
-    parse(File).
+    read_integer_lists(File).
 
-parse(Filename) ->
+index(Rows) ->
+    EnumeratedRows = enumerate([enumerate(Row) || Row <- Rows]),
+    index(EnumeratedRows, _Index = #{}).
+
+index([], Index) -> Index;
+index([{RowIndex, Row} | Rows], Index) ->
+    index(Rows, index_row(RowIndex, Row, Index)).
+
+index_row(_RowIndex, [], Index) -> Index;
+index_row(RowIndex, [{ColIndex, Value} | Cols], Index) ->
+    index_row(RowIndex, Cols, Index#{{RowIndex, ColIndex} => Value}).
+
+read_integer_lists(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
     Lines = string:lexemes(FileContent, "\n"),
-    Lines.
+    [to_integers_list(Line) || Line <- Lines].
+
+to_integers_list(BinInteger) -> [Char - 48 || <<Char>> <= BinInteger].
+
+% Python, I miss you sometimes :'-[
+enumerate(List) ->
+    lists:zip(lists:seq(1, length(List)), List).
