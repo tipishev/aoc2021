08ee72e22fa49653022abc0d8b4e49adf7916112
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-09 09:51:59 +0100

    solve day9 part1

--- src/day9.erl
+++ src/day9.erl
@@ -6,10 +6,36 @@
 part1(File) ->
     Rows = read_integer_lists(File),
     Index = index(Rows),
-    Index.
+    {MaxX, MaxY} = {length(Rows), length(hd(Rows))},
+    Xs = lists:seq(1, MaxX),
+    Ys = lists:seq(1, MaxY),
+    Heights = [maps:get({X, Y}, Index) || X <- Xs, Y <- Ys, is_min(X, Y, Index)],
+    RiskLevels = [H + 1 || H <- Heights],
+    lists:sum(RiskLevels).
+
+is_min(X, Y, Index) ->
+    % io:format("X: ~p, Y: ~p, Index: ~p", [X, Y, Index]),
+    Value = maps:get({X, Y}, Index),
+    Neighbors = [
+        maps:get({Nx, Ny}, Index)
+        || {Nx, Ny} <- neighbors({X, Y}),
+           % a sloppy check for borders
+           maps:is_key({Nx, Ny}, Index)
+    ],
+    BiggerNeighbors = [N || N <- Neighbors, N > Value],
+    BiggerNeighbors =:= Neighbors.
+
+
 
 part2(File) ->
     read_integer_lists(File).
+
+% Neighbors
+% NOTE: does not check the board borders
+neighbors({X, Y}) ->
+    [{NeighbX, NeighbY} || {NeighbX, NeighbY} <- [{X + 1, Y}, {X, Y + 1}, {X - 1, Y}, {X, Y - 1}]].
+
+% Indexer
 
 index(Rows) ->
     EnumeratedRows = enumerate([enumerate(Row) || Row <- Rows]),
@@ -23,6 +49,8 @@ index_row(_RowIndex, [], Index) -> Index;
 index_row(RowIndex, [{ColIndex, Value} | Cols], Index) ->
     index_row(RowIndex, Cols, Index#{{RowIndex, ColIndex} => Value}).
 
+% Parser
+
 read_integer_lists(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
     Lines = string:lexemes(FileContent, "\n"),
@@ -30,6 +58,7 @@ read_integer_lists(Filename) ->
 
 to_integers_list(BinInteger) -> [Char - 48 || <<Char>> <= BinInteger].
 
+% Helpers
+
 % Python, I miss you sometimes :'-[
-enumerate(List) ->
-    lists:zip(lists:seq(1, length(List)), List).
+enumerate(List) -> lists:zip(lists:seq(1, length(List)), List).
