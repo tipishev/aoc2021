87d152706465efbff800a1f0e9d5e831a53def40
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-10 11:53:20 +0100

    solve part 2

--- src/day10.erl
+++ src/day10.erl
@@ -3,11 +3,54 @@
 %% API exports
 -export([part1/1, part2/1]).
 
-part1(File) ->
-    parse(File).
+part1(Filename) ->
+    Lines = parse(Filename),
+    Results = [check(Line) || Line <- Lines],
+    Scores = [score_illegal(Illegal)
+              || {error, {expected, _Expected, got, Illegal}} <- Results],
+    lists:sum(Scores).
 
-part2(File) ->
-    parse(File).
+score_illegal(round) -> 3;
+score_illegal(square) -> 57;
+score_illegal(curly) -> 1197;
+score_illegal(angly) -> 25137.
+
+part2(Filename) ->
+    Lines = parse(Filename),
+    Results = [check(Line) || Line <- Lines],
+    Incomplete = [Stack || {incomplete, Stack} <- Results],
+    Scores = [score_incomplete(Symbols) || Symbols <- Incomplete],
+    SortedScores = lists:sort(Scores),
+    MiddleIndex = round(math:ceil(length(Scores) / 2)),
+    lists:nth(MiddleIndex, SortedScores).
+
+score_completion(round) -> 1;
+score_completion(square) -> 2;
+score_completion(curly) -> 3;
+score_completion(angly) -> 4.
+
+score_incomplete(Symbols) ->
+    lists:foldl(fun(Symbol, Score) -> Score * 5 + score_completion(Symbol) end, 0, Symbols).
+
+check(Tokens) ->
+    check([], Tokens).
+
+check(_Stack = [], _TokensToProcess = []) -> ok;
+check(Stack, _TokensToProcess = []) -> {incomplete, Stack};
+check(Stack, [Token | Tokens]) ->
+    case advance(Stack, Token) of
+        {ok, NewStack} -> check(NewStack, Tokens);
+        {error, Reason} -> {error, Reason}
+    end.
+
+% open is ok at any time, jus push to the stack
+advance(Stack, {open, Type}) -> {ok, [Type | Stack]};
+% closing must match
+advance([Type | Stack], {close, Type}) -> {ok, Stack};
+advance([ExpectedType | _Stack], {close, Type}) -> {error, {expected, ExpectedType, got, Type}};
+advance([], {close, Type}) -> {error, {empty, got_close, Type}}.
+
+% Parser
 
 parse(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
@@ -23,7 +66,4 @@ tokenize(<<"}">>) -> {close, curly};
 tokenize(<<"<">>) -> {open, angly};
 tokenize(<<">">>) -> {close, angly}.
 
-score(round) -> 3;
-score(square) -> 57;
-score(curly) -> 1197;
-score(angly) -> 25137.
+
