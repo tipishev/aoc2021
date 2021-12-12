07fad884645f6d8a7a5f8629fc648b8447f5d404
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-13 00:38:36 +0100

    start labeling vertices

--- src/day12.erl
+++ src/day12.erl
@@ -5,83 +5,36 @@
 
 part1(Filename) ->
     Graph = parse_graph(Filename),
-    find_paths_without_repeating_small_caves(Graph).
+    count_paths_without_repeating_small_caves(Graph).
 
 part2(Filename) ->
-    Graph = parse_graph(Filename),
-    find_paths_with_at_most_one_repeating_small_cave(Graph).
+    _Graph = parse_graph(Filename).
 
 % Graph extensions
 
-find_paths_without_repeating_small_caves(Graph) ->
+get_label(Graph, Vertex) ->
+    {Vertex, Label} = digraph:vertex(Graph, Vertex),
+    Label.
+
+count_paths_without_repeating_small_caves(Graph) ->
     NestedPaths = dfs(Graph, start, _VisitedSmall = ordsets:new(), _CurrentPath = []),
     PathStream = lists:flatten(NestedPaths),
     Paths = unpack(PathStream),
     length(Paths).
 
-find_paths_with_at_most_one_repeating_small_cave(Graph) ->
-    VisitedSmall = ordsets:new(),
-    NestedPaths = dfs2(Graph, start, VisitedSmall, _SecondSmall = undefined, _CurrentPath = []),
-    PathStream = lists:flatten(NestedPaths),
-    Paths = unpack(PathStream),
-    io:format("~p~n", [Paths]),
-    length(Paths).
-
 % Depth-First Traversal (DFS)
 dfs(_Graph, 'end', _VisitedSmall, CurrentPath) -> CurrentPath;
 dfs(Graph, Vertex, VisitedSmall0, CurrentPath) ->
-    VisitedSmall =
-        case is_lower(Vertex) of
-            true -> ordsets:add_element(Vertex, VisitedSmall0);
-            false -> VisitedSmall0;
-            not_applicable -> VisitedSmall0
-        end,
+    % io:format("Label: ~p~n", [get_label(Graph, Vertex)]),
+    Label = get_label(Graph, Vertex),
+    VisitedSmall = case Label of
+                       {small, _Count} -> ordsets:add_element(Vertex, VisitedSmall0);
+                       _ -> VisitedSmall0
+                   end,
     OutNeighbours = out_neighbours(Graph, Vertex),
     VisitableOutNeihbours = ordsets:subtract(OutNeighbours, VisitedSmall),
     [
         dfs(Graph, Neighbour, VisitedSmall, CurrentPath ++ [Neighbour])
-        || Neighbour <- VisitableOutNeihbours
-    ].
-
-% DFS for part 2
-
-% arrived to the 'end'
-dfs2(_Graph, 'end', _VisitedSmall, _SecondSmall, CurrentPath) ->
-    CurrentPath ++ ['end'];
-
-% a small cave has not been visited twice yet
-dfs2(Graph, Vertex, VisitedSmall0, _SecondSmall=undefined, CurrentPath0) ->
-    CurrentPath = CurrentPath0 ++ [Vertex],
-    {VisitedSmall, SecondSmall} =
-        case is_lower(Vertex) of
-            true ->
-                case ordsets:is_element(Vertex, VisitedSmall0) of
-                    % this cave is small and has been visited, so mark it as second small
-                    true -> {VisitedSmall0, Vertex};
-                    % this cave has not been visited befor, second small stays undefined
-                    false -> {ordsets:add_element(Vertex, VisitedSmall0), undefined}
-                end;
-            false ->
-                {VisitedSmall0, undefined}
-        end,
-    % anything goes really
-    OutNeighbours = out_neighbours(Graph, Vertex),
-    VisitableOutNeihbours = ordsets:subtract(OutNeighbours, ['start']),
-    [
-        dfs2(Graph, Neighbour, VisitedSmall, SecondSmall, CurrentPath)
-        || Neighbour <- VisitableOutNeihbours
-    ];
-% a small cave has been visited twice
-dfs2(Graph, Vertex, VisitedSmall0, SecondSmall, CurrentPath0) ->
-    CurrentPath = CurrentPath0 ++ [Vertex],
-    VisitedSmall = case is_lower(Vertex) of
-        true -> ordsets:add_element(Vertex, VisitedSmall0);
-        false -> VisitedSmall0
-    end,
-    OutNeighbours = out_neighbours(Graph, Vertex),
-    VisitableOutNeihbours = ordsets:subtract(OutNeighbours, VisitedSmall),
-    [
-        dfs2(Graph, Neighbour, VisitedSmall, SecondSmall, CurrentPath)
         || Neighbour <- VisitableOutNeihbours
     ].
 
@@ -102,7 +55,22 @@ out_neighbours(Graph, Vertex) ->
     ordsets:from_list(digraph:out_neighbours(Graph, Vertex)).
 
 add_vertices(Graph, Vertices) ->
-    lists:foreach(fun(Vertex) -> digraph:add_vertex(Graph, Vertex) end, Vertices).
+    lists:foreach(fun(Vertex) -> add_vertex(Graph, Vertex) end, Vertices).
+
+% %% @doc add a vertex and label with
+% {Type :: small | big | special, Count :: non_neg_integer | not_applicable}
+add_vertex(Graph, Vertex) ->
+    digraph:add_vertex(Graph, Vertex, label(Vertex)).
+
+label(start) ->
+    {special, not_applicable};
+label('end') ->
+    {special, not_applicable};
+label(Vertex) ->
+    case detect_case(Vertex) of
+        lower -> {small, 0};
+        upper -> {big, not_applicable}
+    end.
 
 add_edges(Graph, Edges) ->
     lists:foreach(fun([Vertex1, Vertex2]) -> add_edge(Graph, Vertex1, Vertex2) end, Edges).
@@ -141,8 +109,12 @@ parse_edge_line(Line) ->
 
 % herlpers
 
-is_lower(start) -> not_applicable;
-is_lower('end') -> not_applicable;
-is_lower(Atom) ->
+detect_case(Atom) ->
     String = atom_to_list(Atom),
-    String =:= string:to_lower(String).
+    Lower = string:to_lower(String),
+    Upper = string:to_upper(String),
+    if
+        String =:= Lower -> lower;
+        String =:= Upper -> upper;
+        String =/= Lower andalso String =/= Upper -> mixed
+    end.
