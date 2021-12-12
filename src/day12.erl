86ca1cae70fa25f20e7efecc7e817c8025d782d0
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-12 13:24:37 +0100

    simplify parser

--- src/day12.erl
+++ src/day12.erl
@@ -4,17 +4,36 @@
 -export([part1/1, part2/1]).
 
 part1(Filename) ->
-    parse(Filename).
+    Graph = parse_graph(Filename),
+    digraph:edges(Graph).
 
 part2(Filename) ->
-    parse(Filename).
+    parse_graph(Filename).
 
-parse(Filename) ->
+% Graph extensions
+
+add_vertices(Graph, Vertices) ->
+    lists:foreach(fun(Vertex) -> digraph:add_vertex(Graph, Vertex) end, Vertices).
+
+add_edges(Graph, Edges) ->
+    lists:foreach(fun([Source, Destination]) ->
+                          digraph:add_edge(Graph, Source, Destination)
+                          % TODO add a Destination -> Source edge, too?
+                  end,
+                  Edges).
+
+% Parse
+
+parse_graph(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
     Lines = string:lexemes(FileContent, "\n"),
-    [parse_line(Line) || Line <- Lines].
+    Edges = [parse_edge_line(Line) || Line <- Lines],
+    Vertices = ordsets:from_list(lists:flatten(Edges)),
+    Graph = digraph:new(),
+    add_vertices(Graph, Vertices),
+    add_edges(Graph, Edges),
+    Graph.
 
-parse_line(Line) ->
-  [Source, Destination] = string:lexemes(Line, "-"),
-  {binary_to_atom(Source), binary_to_atom(Destination)}.
+parse_edge_line(Line) ->
+  [_Src, _Dst] = [binary_to_atom(Token) || Token <- string:lexemes(Line, "-")].
 
