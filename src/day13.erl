7cd1f360b5b0f96acbee6e5b4ef56631a2868c47
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-13 19:29:44 +0100

    add a tear util function

--- src/day13.erl
+++ src/day13.erl
@@ -4,30 +4,65 @@
 -export([part1/1, part2/1]).
 
 part1(Filename) ->
-    {Dots, _Folds} = parse(Filename),
+    {Dots, [Fold | _Folds]} = parse(Filename),
     {MaxX, MaxY} = find_max(Dots),
     Dimensions = {MaxX + 1, MaxY + 1},
     Sheet = sheet_new(Dimensions),
-    sheet_mark(Dots, Sheet).
+    MarkedSheet = mark(Dots, Sheet),
+    % FoldedSheet = fold(MarkedSheet, Fold),
+    {Top, Bottom} = tear(MarkedSheet, 7),
+    show(Top),
+    show(Bottom).
+
 
 part2(Filename) ->
     parse(Filename).
 
 % Sheet
 
-sheet_new({MaxX, MaxY}) -> lists:duplicate(MaxY, lists:duplicate(MaxX, o)).
+% fold(Sheet, {y, Y}) ->
+%     % fold_mark_row(Sheet, Y).
+%     [Top, Bottom] = tear(Sheet, Y).
 
-sheet_mark(Dots, Sheet) when is_list(Dots) ->
-    lists:foldl(fun sheet_mark/2, Sheet, Dots);
-sheet_mark({X0, Y0}, Sheet) ->
+tear(Sheet, Y0) ->
+    Y = Y0 + 1,
+    Top = lists:sublist(Sheet, Y - 1),
+    Bottom = lists:nthtail(Y, Sheet),
+    {Top, Bottom}.
+
+fold_mark_row(Sheet, Y0) ->
+    Y = Y0 + 1,
+    Row = lists:nth(Y, Sheet),
+    PlusRow = [ Dash || Dash <- lists:duplicate(length(Row), '+')],
+    replace(Y, PlusRow, Sheet).
+
+show(Sheet) ->
+    String = lists:flatten([ [ atom_to_list(Cell) || Cell <- Line] ++ "\n" || Line <- Sheet ]),
+    io:format("~s~n", [String]).
+
+sheet_new({MaxX, MaxY}) -> lists:duplicate(MaxY, lists:duplicate(MaxX, '.')).
+
+mark(Dots, Sheet) when is_list(Dots) ->
+    lists:foldl(fun mark/2, Sheet, Dots);
+mark({X0, Y0}, Sheet) ->
     % freaking 0-index
     {X, Y} = {X0 + 1, Y = Y0 + 1},
     Row = lists:nth(Y, Sheet),
-    ModifiedRow = lists:sublist(Row, X - 1) ++ [x] ++ lists:nthtail(X, Row),
-    lists:sublist(Sheet, Y - 1) ++ [ModifiedRow] ++ lists:nthtail(Y, Sheet).
+    ModifiedRow = replace(X, '#', Row),
+    replace(Y, ModifiedRow, Sheet).
 
 find_max(Dots) ->
     lists:foldl(fun({X, Y}, {MaxX, MaxY}) -> {max(X, MaxX), max(Y, MaxY)} end, {0, 0}, Dots).
+
+% Utils
+
+replace(N, Value, List) -> lists:sublist(List, N - 1) ++ [Value] ++ lists:nthtail(N, List).
+
+
+%% stolen from https://stackoverflow.com/a/7855826
+transpose([[]|_]) -> [];
+transpose(M) ->
+    [lists:map(fun hd/1, M) | transpose(lists:map(fun tl/1, M))].
 
 % Parser
 
