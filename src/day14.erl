db5028cbf000b678966a40c8a6a8272ec6205fab
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-14 18:15:46 +0100

    switch hash keys to lists

--- src/day14.erl
+++ src/day14.erl
@@ -4,50 +4,82 @@
 -export([part1/1, part2/1]).
 
 part1(Filename) ->
-    {Template, Rules} = parse(Filename),
-    Polymerized = polimerize(Template, Rules, 10),
-    Values = maps:values(count(Polymerized)),
-    lists:max(Values) - lists:min(Values).
+    {Template, Lookup} = parse(Filename),
+    Polymerized = polimerize(Template, Lookup, 10),
+    max_min_diff(Polymerized).
 
 part2(Filename) ->
-    {Template, Rules} = parse(Filename),
-    Polymerized = polimerize(Template, Rules, 15),
+    {Template, Lookup} = parse(Filename),
+    Polymerized = polimerize(Template, Lookup, 10),
     Values = maps:values(count(Polymerized)),
     lists:max(Values) - lists:min(Values).
 
 % Internal
 
-polimerize(Template, Rules, Times) ->
-    lists:foldl(fun(_Idx, Acc) -> insert(Acc, Rules) end, Template, one_to(Times)).
+max_min_diff(Chain) ->
+    Values = maps:values(count(Chain)),
+    lists:max(Values) - lists:min(Values).
 
-insert(Input = [First | InputTail], Rules) ->
-    Pairs = to_pairs(Input),
-    Insertions = [maps:get(Pair, Rules) || Pair <- Pairs],
-    lists:flatten([First | [[Inserted, Original] || {Inserted, Original} <- lists:zip(Insertions, InputTail)]]).
+polimerize(Chain, _Lookup, 0) -> Chain;
+polimerize(Chain, Lookup, Times) ->
+    {NewChain, NewLookup} = insert(Chain, Lookup),
+    polimerize(NewChain, NewLookup, Times - 1).
 
-to_pairs([]) -> [];
-to_pairs([_]) -> [];
-to_pairs([ A, B | Tail]) -> [{A, B} | to_pairs([B | Tail])].
+insert([], Lookup) -> {[], Lookup};
+insert([A], Lookup) -> {[A], Lookup};
+insert([A, B], Lookup) -> {[A, maps:get([A, B], Lookup), B], Lookup};
+insert(Chain, Lookup) ->
+    % io:format("Chain: ~p~n", [Chain]),
+    % io:format("Lookup: ~p~n", [Lookup]),
+    {Left, Right} = half(Chain),
+    Middle = maps:get([last(Left), first(Right)], Lookup),
+    {NewLeft, _NewLeftLookup} = insert(Left, Lookup),
+    {NewRight, _NewRightLookup} = insert(Right, Lookup),
+    % io:format("NewLeft: ~p~nNewRight:~p~n", [NewLeft, NewRight]),
+    NewChain = NewLeft ++ [Middle] ++ NewRight,
+    % NewLookup = maps:merge(NewLeftLookup, NewRightLookup),
+    NewLookup = Lookup#{Chain => NewChain},
+    {NewChain, NewLookup}.
+
+half(Chain) ->
+    MiddleIndex = length(Chain) div 2,
+    Left = lists:sublist(Chain, MiddleIndex),
+    Right = lists:nthtail(MiddleIndex, Chain),
+    {Left, Right}.
+
+% % %% @doc dumb iterative version that drown on large input.
+% insert(Chain = [ChainHead | ChainTail], Lookup) ->
+%     Pairs = to_pairs(Chain),
+%     Insertions = [maps:get(Pair, Lookup) || Pair <- Pairs],
+%     NewChain = lists:flatten([
+%         ChainHead
+%         | [[Inserted, Original] || {Inserted, Original} <- lists:zip(Insertions, ChainTail)]
+%     ]),
+%     {NewChain, Lookup}.
+
+% to_pairs([]) -> [];
+% to_pairs([_]) -> [];
+% to_pairs([ A, B | Tail]) -> [{A, B} | to_pairs([B | Tail])].
+
+last(List) -> lists:last(List).
+first(List) -> hd(List).
 
 % Parse
 
 parse(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
     [Template | RuleLines] = string:lexemes(string:to_lower(binary_to_list(FileContent)), "\n"),
-    Rules = [parse_rule(Rule) || Rule <- RuleLines],
-    {to_atoms(Template), maps:from_list(Rules)}.
+    Lookup = [parse_rule(Rule) || Rule <- RuleLines],
+    {to_atoms(Template), maps:from_list(Lookup)}.
 
 parse_rule(RuleLine) ->
     [From, To] = string:lexemes(string:to_lower(RuleLine), "- >"),
-    {list_to_tuple(to_atoms(From)), list_to_atom(To)}.
+    {to_atoms(From), list_to_atom(To)}.
 
 to_atoms(String) ->
     [list_to_atom([Char]) || Char <- String].
 
-one_to(Value) -> lists:seq(1, Value).
-
 % A simple counter I reimplement every once in a while :-/
-
 count(List) ->
     count(List, #{}).
 count([], Counter) ->
@@ -56,3 +88,4 @@ count([H | T], Counter) ->
     Increment = fun(N) -> N + 1 end,
     UpdatedCounter = maps:update_with(H, Increment, _Init = 1, Counter),
     count(T, UpdatedCounter).
+
