3a96d0d8f6516f8ba18a6124fee646134939412d
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-15 10:11:42 +0100

    start reworking day14 to frequencies

--- src/day14.erl
+++ src/day14.erl
@@ -5,96 +5,58 @@
 
 part1(Filename) ->
     {Template, Lookup} = parse(Filename),
-    polymerize(Template, Lookup, 10).
-    % max_min_diff(Polymerized).
+    Generator = lookup_to_generator(Lookup),
+    SymbolCounter = polymerize(Template, Generator, 10),
+    max_min_diff(SymbolCounter).
 
 part2(Filename) ->
     {Template, Lookup} = parse(Filename),
-    _Polymerized = polymerize(Template, Lookup, 23).
+    Generator = lookup_to_generator(Lookup),
+    _Polymerized = polymerize(Template, Generator, 23).
     % Values = maps:values(count(Polymerized)),
     % lists:max(Values) - lists:min(Values).
 
-polymerize(Template, _Lookup, _Steps) ->
-    _PairFrequencies = count(to_pairs(Template)),
-    _CharFrequencies = count(Template).
+polymerize(Template, Generator, Steps) ->
+    Pairs = to_pairs(Template),
+    PairFrequencies = count(Pairs),
+    % Updated = update(PairFrequencies, Generator),
+    Updated = lists:foldl(
+                fun(_Idx, FrequencyAcc) -> update(FrequencyAcc, Generator) end,
+                PairFrequencies,
+                lists:seq(1, Steps)),
+    frequency_to_symbol_counter(Updated).
 
-% step(PairFrequencies, )
+update(PairFrequencies, Generator) ->
+    Foo = [{_Multiplier = Frequency,
+      _GeneratedPairs = maps:get(Pair, Generator)}
+     || {Pair, Frequency} <- maps:to_list(PairFrequencies)],
+    Deltas = lists:flatten([[{Pair1, Multiplier}, {Pair2, Multiplier}]
+              || {Multiplier, [Pair1, Pair2]} <- Foo]),
+    lists:foldl(fun frequency_folder/2, #{}, Deltas).
 
-% Internal
-
-% The main part
-
-% polimerize(Chain, _Lookup, _StepsLeft = 0) ->
-%     Chain;
-% polimerize(Chain, Lookup, StepsLeft) ->
-%     io:format("StepsLeft: ~p~n", [StepsLeft]),
-%     {NewChain, NewLookup} = insert(Chain, Lookup),
-%     polimerize(NewChain, NewLookup, StepsLeft - 1).
-
-
-% Graveyard
-
-% insert([], Lookup) -> {[], Lookup};
-% insert([A], Lookup) -> {[A], Lookup};
-% insert([A, B], Lookup) -> {[A, maps:get([A, B], Lookup), B], Lookup};
-% insert(Chain, Lookup) ->
-%     case maps:is_key(Chain, Lookup) of
-%         true ->
-%             {maps:get(Chain, Lookup), Lookup};
-%         false ->
-%             {Body, Butt} = chop_the_butt(Chain),
-%             Middle = maps:get([last(Body), Butt], Lookup),
-%             {NewLeft, NewLeftLookup} = insert(Body, Lookup),
-%             NewChain = NewLeft ++ [Middle] ++ [Butt],
-%             NewLookup = NewLeftLookup#{Chain => NewChain},
-%             {NewChain, NewLookup}
-%     end.
-
-% last(List) -> lists:last(List).
-
-% chop_the_butt(List) ->
-%     [Butt | ReverseBody] = lists:reverse(List),
-%     Body = lists:reverse(ReverseBody),
-%     {Body, Butt}.
-
-% % %% @doc slightly faster solution with halving and reassembling a lookup, drowns at ~25
-% insert([], Lookup) -> {[], Lookup};
-% insert([A], Lookup) -> {[A], Lookup};
-% insert([A, B], Lookup) -> {[A, maps:get([A, B], Lookup), B], Lookup};
-% insert(Chain, Lookup) ->
-%     case maps:is_key(Chain, Lookup) of
-%         true ->
-%             {maps:get(Chain, Lookup), Lookup};
-%         false ->
-%             {Left, Right} = split_in_half(Chain),
-%             Middle = maps:get([last(Left), first(Right)], Lookup),
-%             {NewLeft, NewLeftLookup} = insert(Left, Lookup),
-%             {NewRight, NewRightLookup} = insert(Right, Lookup),
-%             NewChain = NewLeft ++ [Middle] ++ NewRight,
-%             MergedLookup = maps:merge(NewLeftLookup, NewRightLookup),
-%             NewLookup = MergedLookup#{Chain => NewChain},
-%             {NewChain, NewLookup}
-%     end.
-
-% last(List) -> lists:last(List).
-% first(List) -> hd(List).
+frequency_folder({Key, Increase}, Acc) ->
+    Fun = fun(Before) -> Before + Increase end,
+    maps:update_with(Key, Fun, Increase, Acc).
 
 
-% split_in_half(List) ->
-%     MiddleIdx = length(List) div 2,
-%     Left = lists:sublist(List, MiddleIdx),
-%     Right = lists:nthtail(MiddleIdx, List),
-%     {Left, Right}.
+lookup_to_generator(Lookup) ->
+    PairInsertion = maps:to_list(Lookup),
+    PairPairs = [{[First, Second], [[First, Insertion], [Insertion, Second]]}
+                 || {[First, Second], Insertion} <- PairInsertion ],
+    maps:from_list(PairPairs).
 
-% % %% @doc dumb iterative version that drowns on large input.
-% insert(Chain = [ChainHead | ChainTail], Lookup) ->
-%     Pairs = to_pairs(Chain),
-%     Insertions = [maps:get(Pair, Lookup) || Pair <- Pairs],
-%     NewChain = lists:flatten([
-%         ChainHead
-%         | [[Inserted, Original] || {Inserted, Original} <- lists:zip(Insertions, ChainTail)]
-%     ]),
-%     {NewChain, Lookup}.
+frequency_to_symbol_counter(FrequencyCounts) ->
+    Deltas = lists:flatten([ [{Symbol1, Count}, {Symbol2, Count}]
+    || {[Symbol1, Symbol2], Count} <- maps:to_list(FrequencyCounts)]),
+    lists:foldl(fun frequency_folder/2, #{}, Deltas).
+
+
+max_min_diff(SymbolCounter) ->
+    SymbolCounts = maps:to_list(SymbolCounter),
+    CountSymbols = lists:sort([{Count, Symbol} || {Symbol, Count} <- SymbolCounts]),
+    {MinCount, _MinSymbol} = first(CountSymbols),
+    {MaxCount, _MaxSymbol} = last(CountSymbols),
+    MaxCount - MinCount.
 
 % Parse
 
@@ -113,7 +75,7 @@ to_atoms(String) ->
 
 to_pairs([]) -> [];
 to_pairs([_]) -> [];
-to_pairs([ A, B | Tail]) -> [{A, B} | to_pairs([B | Tail])].
+to_pairs([ A, B | Tail]) -> [[A, B] | to_pairs([B | Tail])].
 
 % A simple counter I reimplement every once in a while :-/
 count(List) ->
@@ -124,3 +86,6 @@ count([H | T], Counter) ->
     Increment = fun(N) -> N + 1 end,
     UpdatedCounter = maps:update_with(H, Increment, _Init = 1, Counter),
     count(T, UpdatedCounter).
+
+last(List) -> lists:last(List).
+first(List) -> hd(List).
