1bb6e34ce5e075e9cacbb570292f6dd102ff1299
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-15 07:16:46 +0100

    add day15 inputs and solution stub

---
+++ src/day15.erl
@@ -0,0 +1,15 @@
+-module(day15).
+
+%% API exports
+-export([part1/1, part2/1]).
+
+part1(Filename) ->
+    parse(Filename).
+
+part2(Filename) ->
+    parse(Filename).
+
+parse(Filename) ->
+    {ok, FileContent} = file:read_file(Filename),
+    Lines = string:lexemes(FileContent, "\n"),
+    Lines.
