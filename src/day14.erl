8cad22657885bc8abdcee9ab99610f53f171b63c
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-15 07:15:01 +0100

    start adding pair-counting solution on day14 part 2

--- src/day14.erl
+++ src/day14.erl
@@ -5,49 +5,88 @@
 
 part1(Filename) ->
     {Template, Lookup} = parse(Filename),
-    Polymerized = polimerize(Template, Lookup, 10),
-    max_min_diff(Polymerized).
+    polymerize(Template, Lookup, 10).
+    % max_min_diff(Polymerized).
 
 part2(Filename) ->
     {Template, Lookup} = parse(Filename),
-    Polymerized = polimerize(Template, Lookup, 10),
-    Values = maps:values(count(Polymerized)),
-    lists:max(Values) - lists:min(Values).
+    _Polymerized = polymerize(Template, Lookup, 23).
+    % Values = maps:values(count(Polymerized)),
+    % lists:max(Values) - lists:min(Values).
+
+polymerize(Template, _Lookup, _Steps) ->
+    _PairFrequencies = count(to_pairs(Template)),
+    _CharFrequencies = count(Template).
+
+% step(PairFrequencies, )
 
 % Internal
 
-max_min_diff(Chain) ->
-    Values = maps:values(count(Chain)),
-    lists:max(Values) - lists:min(Values).
+% The main part
 
-polimerize(Chain, _Lookup, 0) -> Chain;
-polimerize(Chain, Lookup, Times) ->
-    {NewChain, NewLookup} = insert(Chain, Lookup),
-    polimerize(NewChain, NewLookup, Times - 1).
+% polimerize(Chain, _Lookup, _StepsLeft = 0) ->
+%     Chain;
+% polimerize(Chain, Lookup, StepsLeft) ->
+%     io:format("StepsLeft: ~p~n", [StepsLeft]),
+%     {NewChain, NewLookup} = insert(Chain, Lookup),
+%     polimerize(NewChain, NewLookup, StepsLeft - 1).
 
-insert([], Lookup) -> {[], Lookup};
-insert([A], Lookup) -> {[A], Lookup};
-insert([A, B], Lookup) -> {[A, maps:get([A, B], Lookup), B], Lookup};
-insert(Chain, Lookup) ->
-    % io:format("Chain: ~p~n", [Chain]),
-    % io:format("Lookup: ~p~n", [Lookup]),
-    {Left, Right} = half(Chain),
-    Middle = maps:get([last(Left), first(Right)], Lookup),
-    {NewLeft, _NewLeftLookup} = insert(Left, Lookup),
-    {NewRight, _NewRightLookup} = insert(Right, Lookup),
-    % io:format("NewLeft: ~p~nNewRight:~p~n", [NewLeft, NewRight]),
-    NewChain = NewLeft ++ [Middle] ++ NewRight,
-    % NewLookup = maps:merge(NewLeftLookup, NewRightLookup),
-    NewLookup = Lookup#{Chain => NewChain},
-    {NewChain, NewLookup}.
 
-half(Chain) ->
-    MiddleIndex = length(Chain) div 2,
-    Left = lists:sublist(Chain, MiddleIndex),
-    Right = lists:nthtail(MiddleIndex, Chain),
-    {Left, Right}.
+% Graveyard
 
-% % %% @doc dumb iterative version that drown on large input.
+% insert([], Lookup) -> {[], Lookup};
+% insert([A], Lookup) -> {[A], Lookup};
+% insert([A, B], Lookup) -> {[A, maps:get([A, B], Lookup), B], Lookup};
+% insert(Chain, Lookup) ->
+%     case maps:is_key(Chain, Lookup) of
+%         true ->
+%             {maps:get(Chain, Lookup), Lookup};
+%         false ->
+%             {Body, Butt} = chop_the_butt(Chain),
+%             Middle = maps:get([last(Body), Butt], Lookup),
+%             {NewLeft, NewLeftLookup} = insert(Body, Lookup),
+%             NewChain = NewLeft ++ [Middle] ++ [Butt],
+%             NewLookup = NewLeftLookup#{Chain => NewChain},
+%             {NewChain, NewLookup}
+%     end.
+
+% last(List) -> lists:last(List).
+
+% chop_the_butt(List) ->
+%     [Butt | ReverseBody] = lists:reverse(List),
+%     Body = lists:reverse(ReverseBody),
+%     {Body, Butt}.
+
+% % %% @doc slightly faster solution with halving and reassembling a lookup, drowns at ~25
+% insert([], Lookup) -> {[], Lookup};
+% insert([A], Lookup) -> {[A], Lookup};
+% insert([A, B], Lookup) -> {[A, maps:get([A, B], Lookup), B], Lookup};
+% insert(Chain, Lookup) ->
+%     case maps:is_key(Chain, Lookup) of
+%         true ->
+%             {maps:get(Chain, Lookup), Lookup};
+%         false ->
+%             {Left, Right} = split_in_half(Chain),
+%             Middle = maps:get([last(Left), first(Right)], Lookup),
+%             {NewLeft, NewLeftLookup} = insert(Left, Lookup),
+%             {NewRight, NewRightLookup} = insert(Right, Lookup),
+%             NewChain = NewLeft ++ [Middle] ++ NewRight,
+%             MergedLookup = maps:merge(NewLeftLookup, NewRightLookup),
+%             NewLookup = MergedLookup#{Chain => NewChain},
+%             {NewChain, NewLookup}
+%     end.
+
+% last(List) -> lists:last(List).
+% first(List) -> hd(List).
+
+
+% split_in_half(List) ->
+%     MiddleIdx = length(List) div 2,
+%     Left = lists:sublist(List, MiddleIdx),
+%     Right = lists:nthtail(MiddleIdx, List),
+%     {Left, Right}.
+
+% % %% @doc dumb iterative version that drowns on large input.
 % insert(Chain = [ChainHead | ChainTail], Lookup) ->
 %     Pairs = to_pairs(Chain),
 %     Insertions = [maps:get(Pair, Lookup) || Pair <- Pairs],
@@ -56,13 +95,6 @@ half(Chain) ->
 %         | [[Inserted, Original] || {Inserted, Original} <- lists:zip(Insertions, ChainTail)]
 %     ]),
 %     {NewChain, Lookup}.
-
-% to_pairs([]) -> [];
-% to_pairs([_]) -> [];
-% to_pairs([ A, B | Tail]) -> [{A, B} | to_pairs([B | Tail])].
-
-last(List) -> lists:last(List).
-first(List) -> hd(List).
 
 % Parse
 
@@ -79,6 +111,10 @@ parse_rule(RuleLine) ->
 to_atoms(String) ->
     [list_to_atom([Char]) || Char <- String].
 
+to_pairs([]) -> [];
+to_pairs([_]) -> [];
+to_pairs([ A, B | Tail]) -> [{A, B} | to_pairs([B | Tail])].
+
 % A simple counter I reimplement every once in a while :-/
 count(List) ->
     count(List, #{}).
@@ -88,4 +124,3 @@ count([H | T], Counter) ->
     Increment = fun(N) -> N + 1 end,
     UpdatedCounter = maps:update_with(H, Increment, _Init = 1, Counter),
     count(T, UpdatedCounter).
-
