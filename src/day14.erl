469539504fab548ea4a59be7941fc49930d11532
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-15 23:16:47 +0100

    solve day14 part 2

--- src/day14.erl
+++ src/day14.erl
@@ -4,70 +4,89 @@
 -export([part1/1, part2/1]).
 
 part1(Filename) ->
-    {Template, Lookup} = parse(Filename),
-    Generator = lookup_to_generator(Lookup),
-    SymbolCounter = polymerize(Template, Generator, 10),
-    max_min_diff(SymbolCounter).
+    {Template, Insertions} = parse(Filename),
+    solve(Template, Insertions, 10).
 
 part2(Filename) ->
-    {Template, Lookup} = parse(Filename),
-    Generator = lookup_to_generator(Lookup),
-    _Polymerized = polymerize(Template, Generator, 23).
-    % Values = maps:values(count(Polymerized)),
-    % lists:max(Values) - lists:min(Values).
+    {Template, Insertions} = parse(Filename),
+    solve(Template, Insertions, 40).
 
-polymerize(Template, Generator, Steps) ->
-    Pairs = to_pairs(Template),
-    PairFrequencies = count(Pairs),
-    % Updated = update(PairFrequencies, Generator),
-    Updated = lists:foldl(
-                fun(_Idx, FrequencyAcc) -> update(FrequencyAcc, Generator) end,
-                PairFrequencies,
-                lists:seq(1, Steps)),
-    frequency_to_symbol_counter(Updated).
+solve(Template, Insertions, Steps) ->
+    PairFrequencies0 = to_pair_frequencies(Template),
+    SymbolFrequencies0 = count(Template),
+    PairGenerator = to_generator(Insertions),
+    SymbolGenerator = maps:from_list(Insertions),
 
-update(PairFrequencies, Generator) ->
-    Foo = [{_Multiplier = Frequency,
-      _GeneratedPairs = maps:get(Pair, Generator)}
-     || {Pair, Frequency} <- maps:to_list(PairFrequencies)],
-    Deltas = lists:flatten([[{Pair1, Multiplier}, {Pair2, Multiplier}]
-              || {Multiplier, [Pair1, Pair2]} <- Foo]),
-    lists:foldl(fun frequency_folder/2, #{}, Deltas).
+    Update = fun({SymbolFrequencies, PairFrequencies}) ->
+        update(
+            SymbolFrequencies,
+            PairFrequencies,
+            SymbolGenerator,
+            PairGenerator
+        )
+    end,
+    {SymbolFrequencies, _PairFrequencies} = lists:foldl(
+      fun(_Idx, Acc) -> Update(Acc) end,
+      {SymbolFrequencies0, PairFrequencies0},
+      lists:seq(1, Steps)
+    ),
+    max_min_diff(SymbolFrequencies).
 
-frequency_folder({Key, Increase}, Acc) ->
-    Fun = fun(Before) -> Before + Increase end,
-    maps:update_with(Key, Fun, Increase, Acc).
+max_min_diff(SymbolFrequencies) ->
+    SortedCounts = lists:sort([Count || {_Symbol, Count} <- maps:to_list(SymbolFrequencies)]),
+    lists:last(SortedCounts) - hd(SortedCounts).
+
+update(SymbolFrequencies0, PairFrequencies0, SymbolGenerator, PairGenerator) ->
+
+    % update pair frequencies
+    PairFrequencyDeltas = lists:flatten([
+        begin
+            [NewPair1, NewPair2] = maps:get(Pair, PairGenerator),
+            [{NewPair1, Multiplier}, {NewPair2, Multiplier}]
+        end
+        || {Pair, Multiplier} <- maps:to_list(PairFrequencies0)
+    ]),
+
+    PairFrequencies = lists:foldl(
+        fun({Pair, Multiplier}, Acc) ->
+            Fun = fun(CurrentCount) -> CurrentCount + Multiplier end,
+            maps:update_with(Pair, Fun, Multiplier, Acc)
+        end,
+        #{},
+        PairFrequencyDeltas
+    ),
+
+    % update symbol frequencies
+    SymbolFrequencyDeltas = [{maps:get(Pair, SymbolGenerator), Multiplier} ||
+               {Pair, Multiplier} <- maps:to_list(PairFrequencies0)],
+    SymbolFrequencies = lists:foldl(
+      fun({Symbol, Multiplier}, Acc) ->
+          Fun = fun(CurrentCount) -> CurrentCount + Multiplier end,
+          maps:update_with(Symbol, Fun, Multiplier, Acc)
+      end,
+      SymbolFrequencies0,
+      SymbolFrequencyDeltas
+    ),
+    {SymbolFrequencies, PairFrequencies}.
 
 
-lookup_to_generator(Lookup) ->
-    PairInsertion = maps:to_list(Lookup),
-    PairPairs = [{[First, Second], [[First, Insertion], [Insertion, Second]]}
-                 || {[First, Second], Insertion} <- PairInsertion ],
-    maps:from_list(PairPairs).
 
-frequency_to_symbol_counter(FrequencyCounts) ->
-    Deltas = lists:flatten([ [{Symbol1, Count}, {Symbol2, Count}]
-    || {[Symbol1, Symbol2], Count} <- maps:to_list(FrequencyCounts)]),
-    lists:foldl(fun frequency_folder/2, #{}, Deltas).
+to_generator(Insertions) ->
+    maps:from_list([{[A, C], [[A, B], [B, C]]} || {[A, C], B} <- Insertions]).
 
-
-max_min_diff(SymbolCounter) ->
-    SymbolCounts = maps:to_list(SymbolCounter),
-    CountSymbols = lists:sort([{Count, Symbol} || {Symbol, Count} <- SymbolCounts]),
-    {MinCount, _MinSymbol} = first(CountSymbols),
-    {MaxCount, _MaxSymbol} = last(CountSymbols),
-    MaxCount - MinCount.
+to_pair_frequencies(Template) ->
+    count(to_pairs(Template)).
 
 % Parse
 
 parse(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
-    [Template | RuleLines] = string:lexemes(string:to_lower(binary_to_list(FileContent)), "\n"),
-    Lookup = [parse_rule(Rule) || Rule <- RuleLines],
-    {to_atoms(Template), maps:from_list(Lookup)}.
+    [Template | InsertionLines] = string:lexemes(string:to_lower(binary_to_list(FileContent)), "\n"),
+    Insertions = [parse_insertion(InsertionLine) || InsertionLine <- InsertionLines],
+    {to_atoms(Template), Insertions}.
 
-parse_rule(RuleLine) ->
-    [From, To] = string:lexemes(string:to_lower(RuleLine), "- >"),
+parse_insertion(InsertionLine) ->
+    [From, To] = string:lexemes(string:to_lower(InsertionLine), "- >"),
     {to_atoms(From), list_to_atom(To)}.
 
 to_atoms(String) ->
@@ -86,6 +105,3 @@ count([H | T], Counter) ->
     Increment = fun(N) -> N + 1 end,
     UpdatedCounter = maps:update_with(H, Increment, _Init = 1, Counter),
     count(T, UpdatedCounter).
-
-last(List) -> lists:last(List).
-first(List) -> hd(List).
