2c53fac26cb6212a70d01337a11133a6662a31ca
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-21 21:53:26 +0100

    switch to {Packet, Tail} API in day16

--- src/day16.erl
+++ src/day16.erl
@@ -4,7 +4,7 @@
 -export([part1/1, part2/1]).
 
 %% debug
--export([decode/1, print/1]).
+-export([decode_hex/1, print/1]).
 
 part1(Filename) ->
     parse(Filename).
@@ -15,41 +15,46 @@ part2(Filename) ->
 packet_type(4) -> literal;
 packet_type(_AnythingBut4) -> operator.
 
+decode_hex(Hex) ->
+    decode(binary:decode_hex(Hex)).
+
 %% decode/1
-decode(<<Version:3, Type:3, Payload/bits>>) ->
+-spec decode(PacketData :: bitstring()) -> {Packet :: map(), Tail :: bitstring()}.
+decode(PacketData) ->
+    <<Version:3, Type:3, Payload/bits>> = PacketData,
     PacketType = packet_type(Type),
-    Decoded = decode(PacketType, Payload),
-    Decoded#{version => Version, packet_type => PacketType}.
+    {Decoded, Tail} = decode(PacketType, Payload),
+    Packet = #{version => Version, packet_type => PacketType, decoded => Decoded},
+    {Packet, Tail}.
 
 %% decode/2
-decode(operator, <<0:1, SubpacketsLength: 15, Payload/bits>>) ->
-    decode(subpackets_length, SubpacketsLength, Payload);
-decode(operator, <<1:1, SubpacketsCount: 11, Payload/bits>>) ->
-    decode(subpackets_count, SubpacketsCount, Payload);
+decode(operator, <<0:1, Length: 15, Subpackets: Length, _Tail/bits>>) ->
+    decode(length, Length, Subpackets);
+decode(operator, <<1:1, Count: 11, Payload/bits>>) ->
+    decode(count, Count, Payload);
 decode(literal, Payload) ->
     #{decoded := Decoded, tail := Tail} = decode(continuous, Payload, _Acc = <<>>),
     Literal = binary:decode_unsigned(pad_to_bytes(Decoded)),
     Consumed = bit_size(Payload) - bit_size(Tail),
-    #{decoded => Literal, tail => Tail, consumed => Consumed}.
+    #{decoded => Literal, consumed => Consumed, tail => Tail}.
 
 % decode/3
-decode(subpackets_length, SubpacketsLength, Payload) ->
-    decode(subpackets_length, SubpacketsLength, Payload, _PacketAcc = []);
-decode(subpackets_count, SubpacketsCount, Payload) ->
-    {subpackets_count, SubpacketsCount, Payload};
+decode(length, Length, Payload) ->
+    decode(length, Length, Payload, _PacketAcc = []);
+decode(count, SubpacketsCount, Payload) ->
+    {not_implemented, subpackets_count, SubpacketsCount, Payload};
+
 decode(continuous, <<0:1, Payload:4, Tail/bits>>, Acc) ->
     #{decoded => <<Acc/bitstring, Payload:4>>, tail => Tail};
 decode(continuous, <<1:1, Payload:4, Continuation/bits>>, Acc) ->
     decode(continuous, Continuation, <<Acc/bitstring, Payload:4>>).
 
 % decode/4
-
-decode(subpackets_length, SubpacketsLength, _Payload, PacketAcc) when SubpacketsLength < 4 ->
-    PacketAcc;
-decode(subpackets_length, SubpacketsLength, Payload, PacketAcc) ->
-    io:format("~p~p~p~p~n", [subpackets_length, SubpacketsLength, Payload, PacketAcc]),
-    Packet = #{tail := Tail, consumed := Consumed} = decode(Payload),
-    decode(subpackets_length, SubpacketsLength - Consumed, Tail, [Packet | PacketAcc]).
+decode(length, RemainingLength, _Payload, Packets) when RemainingLength < 4 ->
+    Packets;
+decode(length, RemainingLength, Payload, Packets) ->
+    {Packet = #{length := PacketLength}, Tail} = decode(Payload),
+    decode(length, RemainingLength - PacketLength, Tail, [Packet | Packets]).
 
 % %% @doc turn a bit string into bytes by prepending enough leading zeros.
 pad_to_bytes(Bitstring) when is_bitstring(Bitstring) ->
