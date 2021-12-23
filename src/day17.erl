469835ec89e8be3dde7d68ade98f142d75340dc4
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-23 17:59:17 +0100

    add drawing

--- src/day17.erl
+++ src/day17.erl
@@ -4,10 +4,43 @@
 -export([part1/1, part2/1]).
 
 part1(Filename) ->
-    parse(Filename).
+    {{x, LowX, HighX}, {y, LowY, HighY}} = parse(Filename),
+    Grid0 = grid(),
+    Grid1 = add(Grid0, {0, 0}, 'S'),
+    Grid2 = add(Grid1, {1, 0}, x),
+    Grid3 = add(Grid2, {0, 1}, y),
+    Grid = add_rectangle(Grid3, {LowX, HighX}, {LowY, HighY}, 'T'),
+    draw(Grid, {min(0, LowX), HighX + 1}, {min(0, LowY - 1), HighY + 8}).
 
 part2(Filename) ->
     parse(Filename).
+
+%%% Grid
+
+grid() -> #{}.
+
+add(Grid, {X, Y}, Value) ->
+    Grid#{{X,Y} => Value}.
+
+add_rectangle(Grid, {LowX, HighX}, {LowY, HighY}, Value) ->
+    Xs = lists:seq(LowX, HighX),
+    Ys = lists:seq(LowY, HighY),
+    XYs = [{X, Y} || X <- Xs, Y <- Ys],
+    lists:foldl(fun({X, Y}, GridAcc) -> add(GridAcc, {X, Y}, Value) end, Grid, XYs).
+
+draw(Grid, {LowX, HighX}, {LowY, HighY}) ->
+    Xs = lists:seq(LowX, HighX),
+    Ys = lists:seq(LowY, HighY),
+    String = lists:flatten(
+        lists:join("\n", lists:reverse([
+            ([atom_to_list(maps:get({X, Y}, Grid, '.')) || X <- Xs])
+            || Y <- Ys
+        ]))
+    ),
+    io:format("~s~n", [String]).
+
+
+%%% Parser
 
 parse(Filename) ->
     {ok, FileContent} = file:read_file(Filename),
