c7712173040bdbf213ba3459e33588a53b637602
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-12-24 20:31:01 +0100

    mark hits with star

--- src/day17.erl
+++ src/day17.erl
@@ -9,7 +9,7 @@ part1(Filename) ->
     Grid0 = grid(),
     Grid1 = add(Grid0, {0, 0}, 'S'),
     Grid2 = add_rectangle(Grid1, {LowX, HighX}, {LowY, HighY}, 'T'),
-    Grid  = step(Grid2, _Position = {0, 0}, _Velocity={6, 9}, Target),
+    Grid = step(Grid2, _Position = {0, 0}, _Velocity = {6, 9}, Target),
     draw(Grid).
 
 part2(Filename) ->
@@ -24,10 +24,19 @@ is_closer(OldPosition, NewPosition, Targets) when is_list(Targets) ->
 is_closer(OldPosition,NewPosition, Target) ->
     distance(NewPosition, Target) < distance(OldPosition, Target) + 10.
 
+
 distance({X1, Y1}, {X2, Y2}) ->
     math:sqrt((X1-X2)*(X1-X2) + (Y1 - Y2)*(Y1-Y2)).
 
 grid() -> #{}.
+
+hit(Grid, Position) ->
+    HitMark = case maps:get(Position, Grid, '.') of
+            '.' -> '#';
+            'T' -> '*';
+            '#' -> '#'
+        end,
+    Grid#{Position => HitMark}.
 
 add(Grid, {X, Y}, Value) ->
     Grid#{{X,Y} => Value}.
@@ -59,21 +68,28 @@ draw(Grid, {LowX, HighX}, {LowY, HighY}) ->
     io:format("~s~n", [String]).
 
 step(Grid0, {X0, Y0}, {Dx0, Dy0}, Target) ->
-    X = X0 + Dx0,
-    Y = Y0 + Dy0,
-    case is_closer({X0, Y0}, {X, Y}, Target) of
+    Position = {X, Y} = {X0 + Dx0, Y0 + Dy0},
+    case is_closer({X0, Y0}, Position, Target) of
         true ->
-            Dx =
-                if
-                    Dx0 > 0 -> Dx0 - 1;
-                    Dx0 < 0 -> Dx0 + 1;
-                    Dx0 =:= 0 -> 0
-                end,
-            Dy = Dy0 - 1,
-            Grid = add(Grid0, {X, Y}, '#'),
+            Grid = hit(Grid0, Position),
+            {Dx, Dy} = update_velocity({Dx0, Dy0}),
             step(Grid, {X, Y}, {Dx, Dy}, Target);
         false -> Grid0
     end.
+
+% is_hit(Target, {X, Y}) ->
+
+update_velocity({Dx0, Dy0}) ->
+    Dx =
+        if
+            Dx0 > 0 -> Dx0 - 1;
+            Dx0 < 0 -> Dx0 + 1;
+            Dx0 =:= 0 -> 0
+        end,
+    Dy = Dy0 - 1,
+    {Dx, Dy}.
+
+
 
 %%% Parser
 
